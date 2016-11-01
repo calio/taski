6849020ec12213b86759740d0f241a2383b77bc6
Jiale Zhi <vipcalio@gmail.com>
2016-10-31 22:31:18 -0700

    Bump version: v0.1.5 → 0.1.6

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ import time
 import yaml
 
 
-VERSION="0.1.5"
+VERSION="0.1.6"
 
 def get_config(args):
     fs = open(args.config)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="taski",
-    version="0.1.5",
+    version="0.1.6",
     author="Jiale Zhi",
     author_email="vipcalio@gmail.com",
     packages=["app"],
